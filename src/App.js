9f54a2d1dfb3bf06f8f620dd7ff7e7310d3d1ebb
Meenakshi <hiringhrbot39@gmail.com>
2023-10-31 09:03:17 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import QRCode from "react-qr-code";
 
 function App() {
+  const [text, setText] = useState("");
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
   return (
-    <div>hi</div>
+    <div className="App">
+      <h1>QR Code Generator</h1>
+      <QRCode value={text} />
+      <h3>Enter your text here 🤓</h3>
+      <div>
+        <input
+          className="input"
+          type="text"
+          value={text}
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+      </div>
+    </div>
   );
 }
 
